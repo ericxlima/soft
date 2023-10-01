a861bc92c5f5395c5a4d70f7b3acb6056d19778e
ericxlima <eric.vinlima@gmail.com>
2023-10-01 08:00:24 -0300

    fix: update models to fix conflict error

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -4,22 +4,17 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Booking extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      Booking.associate = function(models) {
-        Booking.belongsTo(models.Room, {
-            foreignKey: 'roomId',
-            onDelete: 'CASCADE'
-        });
-        Booking.belongsTo(models.User, {
-            foreignKey: 'userId',
-            onDelete: 'CASCADE'
-        });
-     };
+      // Remova a função interna "Booking.associate = function(models) {...}"
+      Booking.belongsTo(models.Room, {
+        foreignKey: 'roomId',
+        onDelete: 'CASCADE',
+        as: 'room'
+      });
+      Booking.belongsTo(models.User, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   Booking.init({
--- backend/models/room.js
+++ backend/models/room.js
@@ -4,18 +4,12 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Room extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      Room.associate = function(models) {
-        Room.hasMany(models.Booking, {
-            foreignKey: 'roomId',
-            as: 'bookings'
-        });
-      };
+      // Remova a função interna "Room.associate = function(models) {...}"
+      Room.hasMany(models.Booking, {
+        foreignKey: 'roomId',
+        as: 'bookings'
+      });
     }
   }
   Room.init({

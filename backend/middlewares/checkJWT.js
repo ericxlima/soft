21509f108a1f6d6019bc65f56aacead3d488a856
ericxlima <eric.vinlima@gmail.com>
2023-10-01 03:47:46 -0300

    feat: create middleware to check JWT and route to profile info

---
+++ backend/middlewares/checkJWT.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+function checkJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, 'SOFT_SECRET_KEY', (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+module.exports = checkJWT;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
+const checkJWT = require('../middlewares/checkJWT');
+
 const router = express.Router();
+
 
 router.post('/register', async (req, res) => {
     try {
@@ -32,5 +35,25 @@ router.post('/login', async (req, res) => {
         res.status(500).send(error.message);
     }
 });
+
+router.get('/profile', checkJWT, async (req, res) => {
+    try {
+        const user = await User.findOne({ where: { id: req.user.sub } });
+
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' });
+        }
+
+        res.send({
+            id: user.id,
+            username: user.username,
+            isAdmin: user.isAdmin
+        });
+
+    } catch (error) {
+        res.status(500).send({ message: 'Server error' });
+    }
+});
+
 
 module.exports = router;

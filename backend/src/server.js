744acf2c33e55ab1fd2a3244a108d6a95292e6b5
ericxlima <eric.vinlima@gmail.com>
2023-10-01 07:58:29 -0300

    chore: install swagger

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,29 @@ const bookingsRoutes = require('../routes/bookings')
 
 const cors = require('cors');
 
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+var swaggerDefinition = {
+    info: {
+        title: 'SOFT',
+        version: '1.0.0',
+        description: 'Sistema de Reserva de Salas'
+    },
+    host: 'localhost:8080',
+    basePath: '/',
+    
+};
+
+var swaggerOptions = {
+    swaggerDefinition,
+    apis: ['../routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+
 const app = express();
 
 app.use(cors());
@@ -17,6 +40,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1/auth', usersRoutes);
 app.use('/api/v1/rooms', roomsRoutes);
 app.use('/api/v1/bookings', bookingsRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to the backend!" });
